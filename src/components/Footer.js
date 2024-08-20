ca79cd63728c4db398b2c0cd5569307c52e6fe62
mayurrohokale <mayurrohokale12345@gmail.com>
2024-08-20 14:19:25 +0530

    created simple parallax effect

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+export default function Footer() {
+  return (
+    <div>
+      <Home />
+      <div className="h-screen bg-gray-200 flex items-center justify-center">
+        <p className="text-2xl">More Content Below</p>
+      </div>
+    </div>
+  );
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,14 @@
 export default function Home(){
 
     return(
-        <h1 className="font-bold">Welcome!</h1>
+        <div className="relative h-screen overflow-hidden">
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-fixed bg-center bg-cover"
+          style={{ backgroundImage: 'url(https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630)' }}
+        />
+        <div className="relative z-10 flex items-center justify-center h-full bg-opacity-50 bg-gray-800">
+          <h1 className="text-4xl font-bold text-white">Beautiful Landscape</h1>
+        </div>
+      </div>
     )
 }
