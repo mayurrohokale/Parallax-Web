a4f867e2756ee18d256a3b2fdf9042ae7792549a
mayurrohokale <mayurrohokale12345@gmail.com>
2024-08-28 18:32:04 +0530

    changed Button styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,18 +10,18 @@ export default function Home() {
       <Parallax strength={600} bgImage={earth1}>
         <div className="content">
           <div className="text-content">
-            <button className="bg-gray-500 text-white px-3 py-1 rounded-md ">
-              Explore Space!
+            <button className=" ">
+              A TRIP TO SPACE !
             </button>
           </div>
         </div>
       </Parallax>
 
-      <Parallax strength={600} bgImage={earth2}>
+      <Parallax strength={600} bgImage={earth2} bgStyle={10}>
         <div className="content">
-          <div className="text-content">Explore Space!</div>
+          <div className="text-button">Explore Space!</div>
           <div className="bg-white p-8">
-            <h1 className="text-center pb-6 ">FIRST CLASS TICKET</h1>
+            <h1 className="text-center pb-6 font-bold ">FIRST CLASS TICKET</h1>
             <h3>
               Humanity has always looked to the skies with wonder, dreaming of
               what lies beyond our blue planet. Today, we stand on the brink of
@@ -39,9 +39,9 @@ export default function Home() {
 
       <Parallax strength={600} bgImage={earth3}>
         <div className="content">
-          <div className="text-content">Explore Space!</div>
+          <div className="text-button">Explore Space!</div>
           <div className="bg-white p-8">
-            <h1 className="text-center pb-6 ">FIRST CLASS TICKET</h1>
+            <h1 className="text-center pb-6 font-bold ">FIRST CLASS TICKET</h1>
             <h3>
               Humanity has always looked to the skies with wonder, dreaming of
               what lies beyond our blue planet. Today, we stand on the brink of
